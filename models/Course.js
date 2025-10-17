6bb036f09860576fea2667308c9050d4587a666a
AryanDhillu <dhilluaryan7@gmail.com>
2025-10-17 12:21:09 +0530

    course schema

--- models/Course.js
+++ models/Course.js
@@ -1,10 +1,14 @@
+// models/Course.js
+
 const mongoose = require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  duration: { type: String, required: true }, // e.g., "8 weeks"
+  duration: { type: String, required: true },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  price: { type: Number, required: true, default: 0 }, // <-- ADD THIS
+  rating: { type: Number, default: 0 } // <-- ADD THIS
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
